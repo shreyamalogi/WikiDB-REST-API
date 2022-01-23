ed2197c1f28d18871b132187d4ccf738721b4f49
SHREYA MALOGI <5hre9a@gmail.com>
2022-01-24 01:22:55 +0530

    version 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-// const { stringify } = require("querystring");
+const { stringify } = require("querystring");
 
 //express app
 const app = express();
@@ -32,7 +32,7 @@ const articleSchema = ({
 const articleModel = mongoose.model("article", articleSchema);
 
 
-//////////////////////////////////////////request targetting articles route aong with cahined routing////////////////////////
+//////////////////////////////////////////request targetting articles route aong with cahined routing///////////////////////////////////////////////////////////////////////
 
 //chained routing
 app.route("/articles")
@@ -70,6 +70,61 @@ app.route("/articles")
     articleModel.deleteMany(function(err) {
         if (!err) {
             res.send("successfully deleted all the articles")
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+
+////////////////////////////////////////   requests targetting a specific article   /////////////////////////////////////////////////////////////////////////////
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res) {
+    const articleTitle = req.params.articleTitle;
+    articleModel.findOne({ title: articleTitle }, function(err, article) {
+        if (article) {
+            const jsonArticle = JSON.stringify(article);
+            res.send(jsonArticle);
+        } else {
+            res.send("No article with that title found.");
+        }
+    });
+})
+
+.patch(function(req, res) {
+    const articleTitle = req.params.articleTitle;
+    articleModel.update({ title: articleTitle }, { content: req.body.newContent },
+        function(err) {
+            if (!err) {
+                res.send("Successfully updated selected article.");
+            } else {
+                res.send(err);
+            }
+        });
+})
+
+.put(function(req, res) {
+
+    const articleTitle = req.params.articleTitle;
+
+    articleModel.update({ title: articleTitle }, { content: req.body.newContent }, { overwrite: true },
+        function(err) {
+            if (!err) {
+                res.send("Successfully updated the content of the selected article.");
+            } else {
+                res.send(err);
+            }
+        });
+})
+
+
+.delete(function(req, res) {
+    const articleTitle = req.params.articleTitle;
+    LostPet.findOneAndDelete({ title: articleTitle }, function(err) {
+        if (!err) {
+            res.send("Successfully deleted selected article.");
         } else {
             res.send(err);
         }
